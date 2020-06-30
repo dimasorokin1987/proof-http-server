638098f750d375bffadc8a803b05bf51d2af74dd
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-07-01 00:20:55 +0300

    Update proof-http-server.go
    rewrite using page type

--- proof-http-server.go
+++ proof-http-server.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-	//"fmt"
-	"io"
+	"fmt"
+	//"io"
 	"net/http"
 	//"html"
-	"log"
+	//"log"
 	"os"
 )
 
+type page {}
+func (p page) serveHTTP (w http.ResponseWriter, _ *http.Request){
+  fmt.Fprint(w,"<h1>hello world")
+}
+
 func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+  var p page
+  err := http.ListenAndServe(":"+os.Getenv("PORT"),p)
+  if(err!=nil)panic(err)
+	//http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		//strReq := html.EscapeString(r.URL.Path)
 		//fmt.Fprintf(w, "Hello, %q\n", strReq)
 		//io.WriteString(w, "Hello from a HandleFunc #1!\n")
-		io.WriteString(w, "status success/n")
-	})
+		//io.WriteString(w, "status success/n")
+	//})
 
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+	//log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
