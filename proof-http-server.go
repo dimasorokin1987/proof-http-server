ce67fb6ba7a6406c552a934c1e7450992d9dd551
Ubuntu <ubuntu@ip-172-31-44-105.ec2.internal>
2020-06-30 18:00:01 +0000

    debug

--- proof-http-server.go
+++ proof-http-server.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"io"
 	"net/http"
-	"html"
+	//"html"
 	"log"
 )
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		strReq := html.EscapeString(r.URL.Path)
-		fmt.Fprintf(w, "Hello, %q\n", strReq)
-		io.WriteString(w, "Hello from a HandleFunc #1!\n")
+		//strReq := html.EscapeString(r.URL.Path)
+		//fmt.Fprintf(w, "Hello, %q\n", strReq)
+		//io.WriteString(w, "Hello from a HandleFunc #1!\n")
+		io.WriteString(w, "HTTP/1.1 404 Not Found\n\n")
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
